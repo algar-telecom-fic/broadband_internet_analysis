1be6f8f938411d7994e6aa2f3fda0885295a678b
ggardusi <gustavo.gardusi@gmail.com>
2018-11-21 17:06:39 -0200

    commit

--- capacity_ports_query/hfc/main.py
+++ capacity_ports_query/hfc/main.py
@@ -9,8 +9,8 @@ def build_excel_file(current_file, previous_file, date_difference):
 	sheet.title = 'Results'
 	build_styles()
 	current_row = 1
-	columns = 10
-	greatest_column = [0 for i in range(columns + 1)]
+	columns = 11
+	greatest_column = [6 for i in range(columns + 1)]
 	sheet.cell(row = current_row, column = 1).value = 'Node'
 	sheet.cell(row = current_row, column = 2).value = 'CMTS'
 	sheet.cell(row = current_row, column = 3).value = 'Qtd. Clientes'
@@ -21,48 +21,52 @@ def build_excel_file(current_file, previous_file, date_difference):
 	sheet.cell(row = current_row, column = 8).value = 'Crescimento mensal (Mbps)'
 	sheet.cell(row = current_row, column = 9).value = 'Previsão de congestionamento (Mês)'
 	sheet.cell(row = current_row, column = 10).value = 'Previsão de congestionamento'
+	sheet.cell(row = current_row, column = 11).value = 'Projeto'
 	for j in range(1, columns + 1):
 		sheet.cell(row = current_row, column = j).style = 'top_style'
 		greatest_column[j] = max(greatest_column[j], len(str(sheet.cell(row = current_row, column = j).value)))
-	for i in database[current_file]:
+	for node_name in database[current_file]:
 		current_row += 1
-		sheet.cell(row = current_row, column = 1).value = i['node']
-		sheet.cell(row = current_row, column = 2).value = '?'
-		sheet.cell(row = current_row, column = 3).value = '?'
-		sheet.cell(row = current_row, column = 4).value = i['capacity']
-		sheet.cell(row = current_row, column = 5).value = i['capacity'] * i['usage'] / 100.0
-		sheet.cell(row = current_row, column = 6).value = i['usage']
-		sheet.cell(row = current_row, column = 7).value = '?'
-		sheet.cell(row = current_row, column = 8).value = '?'
-		sheet.cell(row = current_row, column = 9).value = '?'
-		sheet.cell(row = current_row, column = 10).value = '?'
-		# ~ try:
-			# ~ before = database[previous_file][i.regional][i.locale][i.station][i.cabinet]['occupied']
-			# ~ median = (i.occupied - before) / (date_difference / 30)
-			# ~ sheet.cell(row = current_row, column = 8).value = round(median, 1)
-			# ~ if i.available == 0:
-				# ~ sheet.cell(row = current_row, column = 9).value = 'Esgotado'
-			# ~ elif median > 0:
-				# ~ value = ceil(i.available / median)
-				# ~ if value == 1:
-					# ~ sheet.cell(row = current_row, column = 9).value = 'Esgota em até 1 mês'
-				# ~ elif value > 10:
-					# ~ sheet.cell(row = current_row, column = 9).value = 'Esgota em mais de 10 meses'
-				# ~ else:
-					# ~ sheet.cell(row = current_row, column = 9).value = 'Esgota em até ' + str(value) + ' meses'
-			# ~ elif median < 0:
-				# ~ sheet.cell(row = current_row, column = columns).value = 'Decrescimento'
-			# ~ else:
-				# ~ sheet.cell(row = current_row, column = columns).value = 'Estável'
-		# ~ except Exception as e:
-			# ~ print(e)
-			# ~ sheet.cell(row = current_row, column = 8).value = 'Sem histórico'
-			# ~ sheet.cell(row = current_row, column = 9).value = 'Esgotado' if i.available == 0 else 'Sem histórico'
 		for j in range(1, columns + 1):
 			sheet.cell(row = current_row, column = j).style = 'normal_style'
+		sheet.cell(row = current_row, column = 1).value = node_name
+		sheet.cell(row = current_row, column = 2).value = '?'
+		sheet.cell(row = current_row, column = 3).value = '?'
+		sheet.cell(row = current_row, column = 4).value = database[current_file][node_name]['capacity']
+		sheet.cell(row = current_row, column = 5).value = database[current_file][node_name]['capacity'] * database[current_file][node_name]['usage'] / 100.0
+		sheet.cell(row = current_row, column = 6).value = database[current_file][node_name]['usage'] / 100.0
+		sheet.cell(row = current_row, column = 6).number_format = '0.00%'
+		current_color = 'FF0000' if database[current_file][node_name]['usage'] > 90 else ('FFA500' if database[current_file][node_name]['usage'] > 80 else '00FF00')
+		sheet.cell(row = current_row, column = 6).fill = PatternFill('solid', fgColor = current_color)
+		sheet.cell(row = current_row, column = 11).value = '?'
+		try:
+			before = database[previous_file][node_name]['usage']
+			current = database[current_file][node_name]['usage']
+			median = (current - before) / (date_difference / 30)
+			sheet.cell(row = current_row, column = 7).value = median / 100.0
+			sheet.cell(row = current_row, column = 7).number_format = '0.00%'
+			sheet.cell(row = current_row, column = 8).value = median * database[current_file][node_name]['capacity']
+			if median == 0:
+				sheet.cell(row = current_row, column = 9).value = 0
+				sheet.cell(row = current_row, column = 10).value = 'Estável'
+			else:
+				value = (database[current_file][node_name]['capacity'] - (database[current_file][node_name]['capacity'] * database[current_file][node_name]['usage'] / 100.0)) / (median * database[current_file][node_name]['capacity'])
+				sheet.cell(row = current_row, column = 9).value = value
+				if median < 0:
+					sheet.cell(row = current_row, column = 10).value = 'Decrescimento'
+				else:
+					if value == 1:
+						sheet.cell(row = current_row, column = 10).value = 'Esgota em até 1 mês'
+					elif value > 10:
+						sheet.cell(row = current_row, column = 10).value = 'Esgota em mais de 10 meses'
+					else:
+						sheet.cell(row = current_row, column = 10).value = 'Esgota em até ' + str(value) + ' meses'
+		except Exception as e:
+			print(e)
+			for j in range(7, 11):
+				sheet.cell(row = current_row, column = j).value = 'Sem histórico'
+		for j in range(1, columns + 1):
 			greatest_column[j] = max(greatest_column[j], len(str(sheet.cell(row = current_row, column = j).value)))
-		# ~ current_color = 'FF0000' if i.available == 0 else ('00FF00' if i.available > 10 else 'FFFF00')
-		# ~ sheet.cell(row = current_row, column = 6).fill = PatternFill('solid', fgColor = current_color)
 	for i in range(current_row + 1):
 		sheet.row_dimensions[i + 1].height = 15
 	for j in range(1, columns + 1):
@@ -128,7 +132,7 @@ def read_config_file(filename):
 
 def read_file(filename):
 	global database
-	database[filename] = []
+	database[filename] = {}
 	try:
 		with open(filename, 'r', encoding = 'ISO-8859-1') as input_file:
 			lines = input_file.readlines()
@@ -153,11 +157,10 @@ def read_file(filename):
 					total_capacity += int(v[2])
 					total_usage += float(v[6])
 				median_usage = total_usage / qtd_interfaces
-				database[filename].append({
-					'node': node_name,
+				database[filename][node_name] = {
 					'capacity': total_capacity,
 					'usage': median_usage
-				})
+				}
 	except Exception as e:
 		print(e)
 		print('Failed to read file: ' + filename)
