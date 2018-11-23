14121ea28a0fbcba9a03fac5fa3231af6c1205a8
ggardusi <gustavo.gardusi@gmail.com>
2018-11-23 14:40:15 -0200

    commit

--- capacity_ports_query/hfc/main.py
+++ capacity_ports_query/hfc/main.py
@@ -10,21 +10,21 @@ def build_excel_file(current_file, previous_file, date_difference):
 	build_styles()
 	current_row = 1
 	columns = 11
-	greatest_column = [6 for i in range(columns + 1)]
+	greatest_column = [6 for i in range(columns + 1)]	
 	sheet.cell(row = current_row, column = 1).value = 'Node'
 	sheet.cell(row = current_row, column = 2).value = 'CMTS'
-	sheet.cell(row = current_row, column = 3).value = 'Qtd. Clientes'
+	sheet.cell(row = current_row, column = 3).value = 'Quantidade\nde clientes'
 	sheet.cell(row = current_row, column = 4).value = 'Capacidade (Mbps)'
-	sheet.cell(row = current_row, column = 5).value = 'Utilização média entre as portadoras (Mbps)'
-	sheet.cell(row = current_row, column = 6).value = 'Utilização média entre as portadoras (%)'
-	sheet.cell(row = current_row, column = 7).value = 'Crescimento mensal (%)'
-	sheet.cell(row = current_row, column = 8).value = 'Crescimento mensal (Mbps)'
-	sheet.cell(row = current_row, column = 9).value = 'Previsão de congestionamento (Mês)'
-	sheet.cell(row = current_row, column = 10).value = 'Previsão de congestionamento'
+	sheet.cell(row = current_row, column = 5).value = 'Utilização média\nentre as portadoras (Mbps)'
+	sheet.cell(row = current_row, column = 6).value = 'Utilização média\nentre as portadoras (%)'
+	sheet.cell(row = current_row, column = 7).value = 'Crescimento\nmensal (%)'
+	sheet.cell(row = current_row, column = 8).value = 'Crescimento\nmensal (Mbps)'
+	sheet.cell(row = current_row, column = 9).value = 'Previsão de\ncongestionamento (Mês)'
+	sheet.cell(row = current_row, column = 10).value = 'Previsão de\ncongestionamento'
 	sheet.cell(row = current_row, column = 11).value = 'Projeto'
 	for j in range(1, columns + 1):
 		sheet.cell(row = current_row, column = j).style = 'top_style'
-		greatest_column[j] = max(greatest_column[j], len(str(sheet.cell(row = current_row, column = j).value)))
+		greatest_column[j] = max(greatest_column[j], (len(str(sheet.cell(row = current_row, column = j).value)) + 10) >> 1)
 	for node_name in database[current_file]:
 		current_row += 1
 		for j in range(1, columns + 1):
@@ -33,10 +33,10 @@ def build_excel_file(current_file, previous_file, date_difference):
 		sheet.cell(row = current_row, column = 2).value = '?'
 		sheet.cell(row = current_row, column = 3).value = '?'
 		sheet.cell(row = current_row, column = 4).value = database[current_file][node_name]['capacity']
-		sheet.cell(row = current_row, column = 5).value = database[current_file][node_name]['capacity'] * database[current_file][node_name]['usage'] / 100.0
+		sheet.cell(row = current_row, column = 5).value = round(database[current_file][node_name]['capacity'] * database[current_file][node_name]['usage'] / 100.0, 2)
 		sheet.cell(row = current_row, column = 6).value = database[current_file][node_name]['usage'] / 100.0
 		sheet.cell(row = current_row, column = 6).number_format = '0.00%'
-		current_color = 'FF0000' if database[current_file][node_name]['usage'] > 90 else ('FFA500' if database[current_file][node_name]['usage'] > 80 else '00FF00')
+		current_color = '00FF00' if database[current_file][node_name]['usage'] < 80 else ('FFA500' if database[current_file][node_name]['usage'] < 90 else 'FF0000')
 		sheet.cell(row = current_row, column = 6).fill = PatternFill('solid', fgColor = current_color)
 		sheet.cell(row = current_row, column = 11).value = '?'
 		try:
@@ -45,14 +45,14 @@ def build_excel_file(current_file, previous_file, date_difference):
 			median = (current - before) / (date_difference / 30)
 			sheet.cell(row = current_row, column = 7).value = median / 100.0
 			sheet.cell(row = current_row, column = 7).number_format = '0.00%'
-			sheet.cell(row = current_row, column = 8).value = median * database[current_file][node_name]['capacity']
-			if median == 0:
-				sheet.cell(row = current_row, column = 9).value = 0
+			sheet.cell(row = current_row, column = 8).value = round(median * database[current_file][node_name]['capacity'] / 100.0, 2);
+			if sheet.cell(row = current_row, column = 8).value == 0:
+				sheet.cell(row = current_row, column = 9).value = 'Estável'
 				sheet.cell(row = current_row, column = 10).value = 'Estável'
 			else:
-				value = (database[current_file][node_name]['capacity'] - (database[current_file][node_name]['capacity'] * database[current_file][node_name]['usage'] / 100.0)) / (median * database[current_file][node_name]['capacity'])
+				value = ceil((sheet.cell(row = current_row, column = 4).value - sheet.cell(row = current_row, column = 5).value) / sheet.cell(row = current_row, column = 8).value)
 				sheet.cell(row = current_row, column = 9).value = value
-				if median < 0:
+				if value < 0:
 					sheet.cell(row = current_row, column = 10).value = 'Decrescimento'
 				else:
 					if value == 1:
@@ -67,7 +67,8 @@ def build_excel_file(current_file, previous_file, date_difference):
 				sheet.cell(row = current_row, column = j).value = 'Sem histórico'
 		for j in range(1, columns + 1):
 			greatest_column[j] = max(greatest_column[j], len(str(sheet.cell(row = current_row, column = j).value)))
-	for i in range(current_row + 1):
+	sheet.row_dimensions[1].height = 30
+	for i in range(2, current_row + 1):
 		sheet.row_dimensions[i + 1].height = 15
 	for j in range(1, columns + 1):
 		sheet.column_dimensions[get_column_letter(j)].width = greatest_column[j]
@@ -81,6 +82,7 @@ def build_styles():
 	alignment = Alignment(
 		horizontal = 'center',
 		vertical = 'center',
+		wrap_text = True,
 	)
 	border = Border(
 		left = Side(style = 'thin'),
