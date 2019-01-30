80c1a3754af76c8f6bb39168a2338a79c83494b5
ggardusi <gustavo.gardusi@gmail.com>
2019-01-30 17:41:51 -0200

    commit

--- uberhub/2018/multiplos/sol.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-	int n; scanf("%d", &n);
-	for (int i = 1; i <= 10; ++i)
-		printf("%lld\n", 1LL * i * n);
-	return 0;
-}
--- uberhub/2018/setembro chove/sol.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-	int n; scanf("%*d %d", &n);
-	return printf("%s\n", (n < 50) ? "Setembro faz sol!" : "Setembro chove!"), 0;
-}
