f3afdc7367e2d3ef89e6b73d3583e89570185dff
ggardusi <gustavo.gardusi@gmail.com>
2019-03-25 13:52:53 -0300

    2nd try test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 i=0
-while [ $i -lt 1 ]; do
+while [ $i -lt 360 ]; do
   python3.6 /home/gardusi/github/broadband_internet_analysis/gpon_traffic/main.py
   python3.6 /home/gardusi/github/broadband_internet_analysis/hfc/main.py
   python3.6 /home/gardusi/github/broadband_internet_analysis/xdsl/main.py
