61747354ed4c3fa920a4ba92dcb4791baada8c36
ggardusi <gustavo.gardusi@gmail.com>
2019-03-24 21:43:49 -0300

    read_ports method

--- gpon_traffic/main.py
+++ gpon_traffic/main.py
@@ -3,7 +3,12 @@ import os
 import pymongo
 
 class GPON:
+  convert_status = {
+    'OCUPADO': 'Portas Ocupdas',
+    'VAGO': 'Portas Livres',
+  }
   database = {}
+  date = datetime.datetime.utcnow()
 
   def __init__(self, filepath):
     with open(filepath, 'r', encoding = 'ISO-8859-1') as config_file:
@@ -14,14 +19,35 @@ class GPON:
       self.date_difference = v[3].split('=')[1].strip().split('"')[1].strip()
 
   def read_ports(self):
-    pass
+    with open(self.filepath_ports, 'r', encoding = 'ISO-8859-1') as input_file:
+      for line in input_file.readlines():
+        v = line.split(';')
+        ip = v[ord('Z') - ord('A')].strip()
+        status = v[ord('P') - ord('A')].strip()
+        if ip not in self.database:
+          database[ip] = {
+            'ANEL METRO': '?',
+            'Capacidade_': '?',
+            'Estação': v[ord('R') - ord('A')].strip(),
+            'IP OLT': ip,
+            'Localidade': v[ord('Q') - ord('A')].strip(),
+            'Modelo': v[ord('U') - ord('A')].strip(),
+            'OLT': v[ord('X') - ord('A')].strip(),
+            'Portas Livres': 0,
+            'Portas Ocupdas': 0,
+            'Total Instalado': 0,
+            'Utilização 12/11': '?',
+            'Utilização': '?',
+            'VLAN': v[1 + ord('Z') - ord('A')].strip(),
+          }
+        database[ip]['Total Instalado'] += 1
+        if status in self.convert_status:
+          database[ip][convert_status[status]] += 1
 
 def main():
   gpon = GPON(os.path.dirname(os.path.abspath(__file__)) + '/' + 'config.txt')
   gpon.read_ports()
-  print(gpon.filepath_ports)
-  print(gpon.filepath_current)
-  print(gpon.filepath_previous)
-  print(gpon.date_difference)
+  for i in gpon.database:
+    print(i)
 
 main()
