468b5e1b54d06b480e778222979344202f4ce84e
ggardusi <gustavo.gardusi@gmail.com>
2019-03-25 13:49:21 -0300

    finish test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 i=0
 while [ $i -lt 360 ]; do
-  echo $i
+  python3.6 /home/gardusi/github/broadband_internet_analysis/gpon_traffic/main.py
+  python3.6 /home/gardusi/github/broadband_internet_analysis/hfc/main.py
+  python3.6 /home/gardusi/github/broadband_internet_analysis/xdsl/main.py
   let i+=1
 done
