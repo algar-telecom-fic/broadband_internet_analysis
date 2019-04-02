f72961df2cee62ae3849de661a7d959755e166d7
ggardusi <gustavo.gardusi@gmail.com>
2019-04-02 15:12:41 -0300

    .

--- xdsl/python.py
+++ xdsl/python.py
@@ -8,6 +8,13 @@ def main():
     database = 'kappacidade'
   )
   cursor = db.cursor()
+  cursor.execute(
+    'CREATE TABLE IF NOT EXISTS'
+    + ' xdsl'
+    + ' ('
+      + 'task_id INT AUTO_INCREMENT'
+    + ')'
+  )
   cursor.execute('SHOW TABLES')
   for i in cursor:
     print(i)
