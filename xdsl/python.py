e28638231ecf29228765d40234d75305adf0f7c4
ggardusi <gustavo.gardusi@gmail.com>
2019-04-04 17:21:30 -0300

    .

--- xdsl/python.py
+++ xdsl/python.py
@@ -7,21 +7,14 @@ def main():
     passwd = 'pe',
     database = 'kappacidade'
   )
-  cmd = (
+  cursor = db.cursor()
+  cursor.execute(
     'CREATE TABLE IF NOT EXISTS'
     + ' xdsl'
     + ' ('
-      + 'task_id INT AUTO_INCREMENT,'
-      + 'PRIMARY KEY'
-      + ' ('
-        + 'task_id'
-      + ')'
+      + 'task_id INT AUTO_INCREMENT'
+      + ', PRIMARY KEY (task_id)'
     + ')'
-  )
-  print(cmd)
-  cursor = db.cursor()
-  cursor.execute(
-    cmd
   )
   cursor.execute('SHOW TABLES')
   for i in cursor:
