f00d283c1b3d4c85decb8795b1d08e075efece7d
ggardusi <gustavo.gardusi@gmail.com>
2019-04-10 02:05:07 -0300

    .

--- mysql_gardusi.py
+++ mysql_gardusi.py
@@ -7,7 +7,7 @@ class mySQL:
     print(host)
     print(user)
     print(passwd)
-    print(database)
+    print(database)r
     self.connection = mysql.connector.connect(
       host = host[0],
       user = user[0],
