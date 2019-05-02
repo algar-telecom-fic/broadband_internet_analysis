f90360317d3b93a6973c2bdf500ce3a8d3389d4b
ggardusi <gustavo.gardusi@gmail.com>
2019-05-02 17:42:13 -0300

    debug insert documents

--- gpon/traffic/gpon_traffic.py
+++ gpon/traffic/gpon_traffic.py
@@ -77,12 +77,12 @@ class GPON:
     )
 
   def read_json(self, filepath = str) -> dict:
-    with open(filepath, 'r', encoding = 'latin-1') as file:
+    with open(filepath, 'rb', encoding = 'utf-8') as file:
       return json.load(file)
 
   def read_traffic(self):
     with open(
-      self.config['current_filepath'], 'r', encoding = 'latin-1'
+      self.config['current_filepath'], 'rb', encoding = 'utf-8'
     ) as input_file:
       for line in input_file.readlines():
         v = line.split(';')
@@ -99,7 +99,7 @@ class GPON:
 
   def read_ports(self):
     with open(
-      self.config['ports_filepath'], 'r', encoding = 'latin-1'
+      self.config['ports_filepath'], 'rb', encoding = 'utf-8'
     ) as input_file:
       for line in input_file.readlines():
         v = line.split(';')
