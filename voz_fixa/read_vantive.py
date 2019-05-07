7d7d540bd9a858b3d403e9830ca6ae88c8c5e91e
Pedro Diogo <pedrodiogo219@gmail.com>
2019-05-07 14:46:46 -0300

    optimizing dictionaries

--- voz_fixa/read_vantive.py
+++ voz_fixa/read_vantive.py
@@ -1,79 +1,62 @@
 from datetime import date
 from DatabaseConnector import DatabaseConnector
+from mydict import InfiniteDict
 
 
 def read_vantive(filename=None):
-    database = {}
-    possible_status = []
+    database = InfiniteDict()
+    possible_status = set()
+
     with open(filename, 'r', encoding='ISO-8859-1') as input_file:
         next(input_file)
 
         for line in input_file.readlines():
             attributes = line.strip().split(';')
+            quantidade, areaLocal, localidade, status, tecnologia = attributes
 
-            quantidade = attributes[0];
-            areaLocal  = attributes[1];
-            localidade = attributes[2];
-            status     = attributes[3];
-            tecnologia = attributes[4];
+            possible_status.add(status)
 
-            if status not in possible_status:
-                possible_status.append(status)
+            database[areaLocal][localidade][tecnologia][status] = database[areaLocal][localidade][tecnologia].get(status, 0) + int(quantidade)
 
-            if localidade not in database:
-                database[localidade] = {'area local' : areaLocal}
-
-            if tecnologia not in database[localidade]:
-                database[localidade][tecnologia] = {}
-
-            if status not in database[localidade][tecnologia]:
-                database[localidade][tecnologia][status] = 0
-
-            database[localidade][tecnologia][status]+=int(quantidade)
-
-    possible_status.sort()
-
+    possible_status = sorted(possible_status)
 
     hoje = date.today()
 
     lista_vantive = []
-
-    for cidade in sorted(database):
-        for tecnologia in database[cidade]:
-            if tecnologia == 'area local': continue
-
-            item = [cidade, database[cidade]['area local'], tecnologia]
-
-            for status in possible_status:
-                if status not in database[cidade][tecnologia]:
-                    item.append(0)
-                else:
-                    item.append(database[cidade][tecnologia][status])
-
-            item.append(hoje)
-
-            lista_vantive.append(tuple(item))
+    for areaLocal, dados in database.items():
+        for localidade, dados in dados.items():
+            for tecnologia in dados:
+                data = [areaLocal, localidade, tecnologia]
+                for status in possible_status:
+                    data.append( database[areaLocal][localidade][tecnologia].get(status, 0) )
+                data.append(hoje)
+                lista_vantive.append(tuple(data))
 
     return (possible_status, lista_vantive)
+    
 
-
-def processVantive(filename=None):
+def processVantive(filename=None, dbcofigfile=None):
     possible_status, data = read_vantive(filename);
 
     query = (
-        'INSERT INTO area_local(cidade, areaLocal, tecnologia, ' + \
+        'INSERT INTO area_local(areaLocal, cidade, tecnologia, ' + \
         ', '.join( [(f"`{i}`") for i in possible_status]) + \
         ', dia)' + 'VALUES (' + \
         ', '.join(["%s" for i in range(0, len(possible_status)+4)]) + ')'
     )
 
-    print(query)
     db = DatabaseConnector()
-    db.configureDB('dbconfigs.env')
+    db.configureDB(dbconfigfile)
     db.executaQuery(query, data)
 
+def testaVantive(filename):
+    status, data = read_vantive(filename)
+    print(status)
+    for d in data:
+        print(d)
 
 if __name__ == '__main__':
     filename = 'datasheets/Relatorio_Area_Local09042019080046.csv'
-    #filename = 'datasheets/area_local_minimizado.csv'
-    processVantive(filename)
+    dbconfigfile = 'dbconfigs.env'
+    processVantive(filename, dbconfigfile)
+    #testaVantive(filename)
