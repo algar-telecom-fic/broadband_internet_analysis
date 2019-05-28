98cb65d5a86fa92d9b2c4e09bca883b4f9dea7f6
pediogo <pediogo@lxmrtg.(none)>
2019-05-28 13:35:09 -0300

    estado do servidor

--- interface/config.py
+++ interface/config.py
@@ -1,7 +1,7 @@
 import os
 
 DEBUG = True
-UPLOAD_FOLDER = '/home/pediogo/Documentos/pastaDosUploads'
+UPLOAD_FOLDER = '/home/pediogo/broadband_internet_analysis/uploads'
 PATH_GPON_PORTAS = '/home/pediogo/Documentos/broadband_internet_analysis/gpon/ports'
 ALLOWED_EXTENSIONS = ['csv', 'xlsx']
 SECRET_KEY = os.urandom(16)
