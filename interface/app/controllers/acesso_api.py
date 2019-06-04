808a1cbb71b117782bf906d305bbde7099a6671d
Pedro Diogo <pedrodiogo219@gmail.com>
2019-06-04 16:41:53 -0300

    updating acesso page text

--- interface/app/controllers/acesso_api.py
+++ interface/app/controllers/acesso_api.py
@@ -9,15 +9,10 @@ import sys
 @app.route("/voz_fixa/acesso", methods=['GET', 'POST'])
 def acesso():
     if request.method == 'POST':
-
-
-
         configs = read_json(os.path.abspath('../voz_fixa/acesso/files/config.json'))
         file1 = make_one_upload(request, configs["actual_filepath"])
         file2 = make_one_upload(request, configs["base_filepath"])
         file3 = make_one_upload(request, configs["regional_filepath"])
-
-
 
         sys.path.append(os.path.abspath('../voz_fixa/acesso'))
         import main
