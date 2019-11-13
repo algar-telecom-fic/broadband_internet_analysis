03b6e185cf3ffbf4fc824ba1dcd948d2cb06699a
Otsuka <gabrielotsuka@gmail.com>
2019-11-13 15:10:13 -0300

    Tratamento das tabelas e suas especificidades

--- spo/main.py
+++ spo/main.py
@@ -1,53 +1,83 @@
-# dict = {
-# 	"table_name":       ,
-# 	"table_id":			,
-# 	"max_tuple_num":	,
-# 	"used_num"
-# }
+import json
+import os
+import datetime
+import sys
+sys.path.append("/home/otsuka/doing/spo")
+from my_sql import mySQL
 
-# final_answer = [list(public) + list(private)]
-
-# Criar função genérica para tratar cada tipo de tabela.
-
-# def treat_line(line, pub, pri):
-# 	aux = line.split()
-# 	if len(aux) == 4:
-# 		if aux in pub:				# Mudar para Busca Binária
-# 			pass
-# 		else:
-# 			pub.append(aux)
-# 	else:
-
-def treat_table(table, pub, pri):
-	print()
+def aux_treat(table, interval, tod):
+	ans = {}
+	ans['Table_name'] = table[0][0]
+	ans['Table_id'] = int(table[0][1])
+	ans['Maximum_Tuple_Number'] = 0
+	ans['Used_number'] = 0
+	ans['Dia'] = tod
 	for i in table:
-		print(i)
-	print()
+		if len(i) < 5:
+			continue
+		elif int(i[2]) >= interval[0] and int(i[2]) <= interval[1]:
+			ans['Maximum_Tuple_Number'] += int(i[3])
+			ans['Used_number'] += int(i[4])
+	return ans
 
-		
-def main():
-	dirt = "/home/otsuka/Documents/Projects/doing/spo/"
-	pub = []
-	pri = []
-	# Lista que contém temporariamente as tabelas a serem tratadas
+
+def treat_table(table, tod):
+	ans = {}
+	spec = info[str(table[0][1])]
+	if spec == 1 or spec == 2:
+		ans['Table_name'] = table[0][0]
+		ans['Table_id'] = int(table[0][1])
+		ans['Maximum_Tuple_Number'] = int(table[0][2])
+		ans['Used_number'] = int(table[0][3])
+		ans['Dia'] = tod
+	elif spec == 3:
+		ans = aux_treat(table, [1000, 1006], tod)
+	elif spec == 4:
+		ans = aux_treat(table, [1500, 1506], tod)
+	elif spec == 5:
+		ans = aux_treat(table, [1700, 1701], tod)
+	fans.append(ans)
+
+
+def main(filename, tod):
+	flag_fst = True
+	dirt = os.getcwd()
+	aux = []
 	table = []
-	with open(dirt + "TABELA-SPO-22-08-2019.txt", "r") as file:
-		for idx, line in enumerate(file):
-			# Ignorar a linha com o nome das colunas
-			if idx == 0:
-				continue
-			# Acumular as linhas até que a ID da tabela (line[1]) seja diferente.
-			# Nesse caso temos o fim de uma tabela e a tratamos.
-			elif idx == 1:
-				table.append(line.split())
-			else: 
-				if line.split()[1] != table[0][1]:
-					treat_table(table, pub, pri)
-					table = []
-				table.append(line.split())
-		# Ao final a última tabela também deve ser tratada, já q nunca entrará no if.
-		treat_table(table,pub,pri)
+
+	with open(dirt + "/files/" + filename, "r") as file:
+		for line in file:
+			if line[:4] == ' tbl' or line[:4] == ' TBL':
+				aux.append(line.split())
+		aux.sort()
+
+	table.append(aux[0])
+	for teste in aux[1:]:
+		if teste[1] != table[0][1]:
+			treat_table(table, tod)
+			table = []
+		table.append(teste)
+	# Ao final, a última tabela também deve ser tratada, já q nunca entrará no if.
+	treat_table([teste], tod)
+
+	spo_files = read_json(dirt + "/files/spo_config.json")
+	filepath = spo_files ["database_credentials"]
+	credentials = read_json(filepath)
+
+	db_name = spo_files ["database_name"]
+	table_name = spo_files ["table_name"]
+	table_info = spo_files ["table_info"]
+	spo_info = read_json(table_info)
+	db = mySQL(credentials, db_name)
+	db.insert_into(table_name, spo_info, fans)
 
 
+def read_json(filepath):
+	with open(filepath, 'r') as file:
+		return json.loads(file.read(), encoding = 'utf-8')
+
+
+fans = []
+info = read_json(os.getcwd() + "/files/tables.json")
 if __name__ == "__main__":
-	main()
+	main("TABELAS SPO - 21-10-2019.txt", datetime.date(2019, 10, 11))
